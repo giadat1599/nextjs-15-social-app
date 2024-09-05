019ba1d7c8b7b510aa01ae6ebe4f951ff88d865a
Gia Dat Truong <giadattruong@192.168.2.3>
2024-09-05 15:59:20 +0700

    feat: following feed

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -2,13 +2,26 @@ import PostEditor from "../../components/posts/editor/post-editor";
 
 import TrendingSidebar from "@/components/trending-sidebar/trending-sidebar";
 import ForYouFeed from "./components/for-you-feed";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import FollowingFeed from "./components/following-feed";
 
 export default function Home() {
   return (
     <main className="flex w-full min-w-0 gap-5">
       <div className="w-full min-w-0 space-y-5">
         <PostEditor />
-        <ForYouFeed />
+        <Tabs defaultValue="for-you">
+          <TabsList>
+            <TabsTrigger value="for-you">For you</TabsTrigger>
+            <TabsTrigger value="following">Following</TabsTrigger>
+          </TabsList>
+          <TabsContent value="for-you">
+            <ForYouFeed />
+          </TabsContent>
+          <TabsContent value="following">
+            <FollowingFeed />
+          </TabsContent>
+        </Tabs>
       </div>
       <TrendingSidebar />
     </main>
